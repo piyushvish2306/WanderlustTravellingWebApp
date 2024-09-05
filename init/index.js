f0d3f6149da9fc448b79c3310861fddb3756c3ff
Piyush Vishwakarma <145245382+piyushvish2306@users.noreply.github.com>
2024-09-05 14:27:30 +0530

    Create index.js

---
+++ init/index.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const initdata = require("./data.js");
+const Listing = require("../models/listing.js"); 
+
+const MONGO_URL = ("mongodb://127.0.0.1:27017/wanderlust");
+
+main()
+    .then(() => {
+        console.log("connected to DB");
+    })
+    .catch((err) =>{
+        console.log(err);
+    });
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () => {
+    await Listing.deleteMany({});
+    initdata.data = initdata.data.map((obj) => ({
+        ...obj,
+        owner: "66c2b36ba1c098e376327d5c"
+    })
+    );
+    await Listing.insertMany(initdata.data);
+    console.log("data was initialised");
+};
+
+
+initDB();
